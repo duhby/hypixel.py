c955878f46da0c297d4d154a04ab5ba5c0816cc5
dubs <59372145+duhby@users.noreply.github.com>
2021-06-02 18:04:12 -0400

    Create __init__.py

---
+++ hypixel/__init__.py
@@ -0,0 +1,16 @@
+"""
+hypixel.py
+~~~~~~~~~~~~~~~~~~~
+
+A python wrapper for the Hypixel API
+
+(c) 2021 duhby
+MIT License, see LICENSE for more details.
+
+"""
+
+__title__ = 'hypixel'
+__author__ = 'duhby'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 duhby'
+__version__ = '0.0.0'
