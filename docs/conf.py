9f911641c0a4fb24be3f96312a341ed1105b906d
dubs <59372145+duhby@users.noreply.github.com>
2021-06-02 21:54:11 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,3 @@
-# Heavily inspired by Rapptz' discord.py docs:
-# https://github.com/Rapptz/discord.py/blob/master/docs/conf.py
-
 # Configuration file for the Sphinx documentation builder.
 
 # -- Path setup --------------------------------------------------------------
@@ -10,13 +7,12 @@ import sys
 from datetime import datetime
 
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.append(os.path.abspath('extensions'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = "hypixel.py"
-copyright = f"2021-{datetime.now().year}, dubs"
+copyright = f"2021-{datetime.now().year}, duhby"
 
 release = "0.0.1"
 
@@ -24,36 +20,22 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-#     'builder',
+    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.extlinks',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinxcontrib_trio',
-#     'details',
-#     'attributetable',
 ]
 
 autodoc_member_order = 'bysource'
-autodoc_typehints = 'none'
 
-# templates_path = ['_templates']
-source_suffix = '.rst'
-master_doc = 'index'
-branch = 'master'
 language = 'en'
-exclude_patterns = ['_build', '_templates']
+exclude_patterns = ['_build']
 pygments_style = 'friendly'
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = 'basic'
+html_theme = 'sphinx_rtd_theme'
 
 html_favicon = './images/icon.png'
-
-html_js_files = [
-  'copy.js',
-  'custom.js',
-  'settings.js',
-  'sidebar.js'
-]
