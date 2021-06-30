fe9ae7947b0ff5d9f0e8af56d3e7a4d3cb8aab5d
dubs <59372145+duhby@users.noreply.github.com>
2021-06-30 18:34:12 -0400

    Create __init__.py

---
+++ hypixel/models/__init__.py
@@ -0,0 +1,12 @@
+"""
+hypixel.models
+~~~~~~~~~~~~~~~~~~~
+
+A module used for internally creating models.
+
+(c) 2021-present duhby
+MIT License, see LICENSE for more details.
+"""
+
+__all__ = (
+)
