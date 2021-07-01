29fa02117b8ee56d69f83032ef7e441f404ff5c3
dubs <59372145+duhby@users.noreply.github.com>
2021-07-01 13:58:25 -0400

    Create __init__.py

---
+++ hypixel/constants/__init__.py
@@ -0,0 +1,12 @@
+"""
+hypixel.models
+~~~~~~~~~~~~~~~~~~~
+
+A module used for cleanly storing alias data.
+
+(c) 2021-present duhby
+MIT License, see LICENSE for more details.
+"""
+
+__all__ = (
+)
