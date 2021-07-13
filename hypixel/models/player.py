f76143f7bab17605bfeb92a0f8ce8dcd3bb48e21
dubs <59372145+duhby@users.noreply.github.com>
2021-07-12 23:38:26 -0400

    Add documentation for Player model in player.py

--- hypixel/models/player.py
+++ hypixel/models/player.py
@@ -23,6 +23,7 @@ DEALINGS IN THE SOFTWARE.
 """
 
 from dataclasses import dataclass
+
 from datetime import datetime
 from typing import Optional
 from typing import List
@@ -44,6 +45,63 @@ __all__ = (
 
 @dataclass
 class Player:
+    """Base model for player stats.
+
+    Attributes
+    ----------
+    raw: Dict[str, Any]
+        The raw json response data.
+    response: ClientResponse
+        The raw response object returned from the API.
+    id: str
+        The Hypixel ID of the player.
+    uuid: str
+        The Mojang UUID of the player.
+    first_login: datetime
+        The first login time of the player.
+    name: str
+        The Minecraft username of the player.
+
+        .. note::
+
+            This won't always be up to date with Mojang's servers, as it
+            only updates when the player logs on Hypixel.
+    last_login: datetime
+        The last login time of the player.
+    last_logout: datetime
+        The last logout time of the player.
+    known_aliases: List[str]
+        A list of known past usernames.
+    achievements: List[Achievement]
+        A list of current achievements the player has.
+    achievement_points: int:
+        The currnet amount of achievement points the player has.
+    network_exp: int
+        The current amount of network experience the player has.
+    karma: int
+        The current amount of karma the player has.
+    version: Optional[str]
+        The last Minecraft version the player logged in using.
+
+        .. warning::
+
+            Sometimes returns ``None`` due to the fact that players
+            could have logged into the server with an unrecognized
+            server protocol at the time which doesn't want to fix
+            itself afterwards deeming this value as unstable.
+    current_gadget: Optional[str]
+        The current gadget the player has equipped.
+    channel: Optional[str]
+        The current channel the player is in.
+    most_recent_game: Optional[Game]
+        The most recent game type of the player.
+    level: float
+        The current network level of the player. Up to 2 decimal places.
+    socials: Socials
+        A :class:`Socials` object of the player's linked social accounts.
+    parkour: Parkour
+        A :class:`Parkour` object of the player's parkour times.
+    """
     _data: Dict[str, Any]
     raw: Dict[str, Any]
     response: ClientResponse
@@ -51,20 +109,20 @@ class Player:
     uuid: str
     first_login: datetime
     name: str
-    last_login: datetime = None
-    last_logout: datetime = None
-    known_aliases: List[str] = None
-    achievements: List[Achievement] = None
-    network_exp: int = 0
-    karma: int = 0
+    last_login: datetime
+    last_logout: datetime
+    known_aliases: List[str]
+    achievements: List[Achievement] # should always exist because of 'general_first_join'
+    achievement_points: int
+    network_exp: int
+    karma: int
     version: str = None
-    achievement_points: int = 0
     current_gadget: str = None
     channel: str = None
     most_recent_game: Game = None
 
     def __post_init__(self):
-        # stats - use individual games instead of one stats class
+        # todo: use individual games instead of one stats class
         # stats = utils._clean(self._data, mode='stats')
         # stats['_data'] = self._data
         # self.stats: Stats = Stats(**stats)
