6d2e4555fecdbcba3e0c99444f94cbd1481332d5
dubs <59372145+duhby@users.noreply.github.com>
2021-11-25 17:30:41 -0500

    Create __init__.py

---
+++ hypixel/models/playerdata/__init__.py
@@ -0,0 +1,13 @@
+"""
+hypixel.models.playerdata
+~~~~~~~~~~~~~~~~~~~
+
+A module used for modeling player specific data.
+
+(c) 2021-present duhby
+MIT License, see LICENSE for more details.
+"""
+
+from .arcade import Arcade
+from .bedwars import Bedwars
+from .socials import Socials
