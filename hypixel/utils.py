ddb98cefe98394bebc3de72f9fe3c1942254829e
dubs <59372145+duhby@users.noreply.github.com>
2021-11-25 17:19:32 -0500

    Update functions

--- hypixel/utils.py
+++ hypixel/utils.py
@@ -22,17 +22,58 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-from typing import Dict
+from typing import Dict, Union
+from aiohttp import ClientSession
 
-def _clean(data: Dict, mode=None) -> Dict:
-    if mode is None:
-        aliases = Constants.ALIASES
-    else:
-        aliases = Constants.GAMEMODES[mode]
-    # replaces keys in data with their formatted alias
-    replaced_data = {aliases.get(k, k): v for k, v in data.items()}
-    # removes unused/unsupported items
-    return {key: replaced_data[key] for key in replaced_data if key in aliases.values()}
+from .constants import aliases
+
+def _clean(data: Dict, mode='PLAYER') -> Dict:
+    alias = getattr(aliases, mode)
+    if mode == 'PLAYER':
+        # avoid name conflicts
+        data['achievement_stats'] = data.pop('achievements', {})
+    elif mode == 'BEDWARS':
+        _data = dict(data)
+        level = data.get('achievement_stats', {}).get('bedwars_level')
+        data = data.get('stats', {}).get('Bedwars', {})
+        data['bedwars_level'] = level
+        data['_data'] = _data
+    elif mode in ('ARCADE', 'HYPIXEL_SAYS', 'PARTY_GAMES'):
+        _data = dict(data)
+        data = data.get('stats', {}).get('Arcade', {})
+        data['_data'] = _data
+    elif 'BEDWARS' in mode:
+        data = data.get('stats', {}).get('Bedwars', {})
+    elif mode == 'CTW':
+        data = data.get('achievement_stats', {})
+    elif mode == 'TKR':
+        data = data.get('stats', {}).get('GingerBread', {})
+    # replace keys in data with their formatted alias
+    replaced_data = {alias.get(k, k): v for k, v in data.items()}
+    # remove unused/unsupported items
+    return {key: replaced_data[key] for key in replaced_data if key in alias.values()}
 
 def get_level(network_exp: int) -> float:
-    return 1 + (-8750.0 + (8750 ** 2 + 5000 * network_exp) ** 0.5) / 2500
+    return round(1 + (-8750.0 + (8750 ** 2 + 5000 * network_exp) ** 0.5) / 2500, 2)
+
+async def _get_uuid(session: ClientSession, name: str) -> str:
+    try:
+        response = await session.get(f'https://api.mojang.com/users/profiles/minecraft/{name}')
+        # TODO: add response error handling
+        response = await response.json()
+        return response.get('id')
+    except Exception:
+        raise ValueError
+
+def safe_div(a: int, b: int) -> float:
+    if not b:
+        return a
+    else:
+        return round(a / b, 2)
+
+COLORS = ('§0', '§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§a', '§b', '§c', '§d', '§e', '§f')
+def clean_rank_prefix(string: str) -> str:
+    for color in COLORS:
+        string = string.replace(color, '')
+    string = string.replace('[', '').replace(']', '')
+    return string
