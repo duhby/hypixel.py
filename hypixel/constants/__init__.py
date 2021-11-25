44e2c733297411bebe0d447db5129d1238dd5c48
dubs <59372145+duhby@users.noreply.github.com>
2021-11-25 17:10:25 -0500

    Fix formatting

--- hypixel/constants/__init__.py
+++ hypixel/constants/__init__.py
@@ -1,12 +1,9 @@
 """
-hypixel.models
+hypixel.constants
 ~~~~~~~~~~~~~~~~~~~
-
-A module used for cleanly storing alias data.
-
+A module used for storing constant data.
 (c) 2021-present duhby
 MIT License, see LICENSE for more details.
 """
 
-__all__ = (
-)
+from . import aliases
