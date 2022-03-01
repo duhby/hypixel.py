f3e6733c913e5aaae64971e1483dc7d5aed25441
dubs <59372145+duhby@users.noreply.github.com>
2022-03-01 11:38:23 -0700

    Create player_count.py

---
+++ examples/player_count.py
@@ -0,0 +1,17 @@
+# This exmaple requies an api key
+
+import hypixel
+import asyncio
+
+description = """A simple example that shows how to get the current number of players."""
+
+async def main():
+    client = hypixel.Client('api-key')
+    async with client:
+        try:
+            print(client.player_count())
+        except HypixelException as error:
+            print(error)
+
+if __name__ == '__main__':
+    asyncio.run(main())
