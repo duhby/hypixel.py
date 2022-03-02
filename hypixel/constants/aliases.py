168f071917df62346bfc9cadc553d8534c89a6b6
dubs <59372145+duhby@users.noreply.github.com>
2022-03-01 20:00:19 -0700

    Add more things

--- hypixel/constants/aliases.py
+++ hypixel/constants/aliases.py
@@ -22,6 +22,25 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
+REQUIRE_COPY = (
+    'ARCADE',
+    'DUELS',
+    'HYPIXEL_SAYS',
+    'MINI_WALLS',
+    'PARTY_GAMES',
+)
+
+# even if key-value pairs are equal, they still need to be added because
+# values that don't have a key in the dictionary are skipped
+
+KEY = {
+    'key': 'key',
+    'owner': 'owner',
+    'limit': 'limit',
+    'totalQueries': 'queries',
+    'queriesInPastMin': 'recent_queries',
+}
+
 PLAYER = {
     '_id': 'id',
     'uuid': 'uuid',
@@ -160,6 +179,17 @@ PARTY_GAMES = {
     'wins_party_3': 'wins_3',
 }
 
+MINI_WALLS = {
+    'kills_mini_walls': 'kills',
+    'deaths_mini_walls': 'deaths',
+    'wins_mini_walls': 'wins',
+    'final_kills_mini_walls': 'final_kills',
+    'wither_kills_mini_walls': 'wither_kills',
+    'wither_damage_mini_walls': 'wither_damage',
+    'arrows_hit_mini_walls': 'arrows_hit',
+    'arrows_shot_mini_walls': 'arrows_shot',
+}
+
 TKR = {
     'coins': 'coins',
     'laps_completed': 'laps',
@@ -170,8 +200,178 @@ TKR = {
     'banana_hits_sent': 'banana_hits',
     'banana_hits_received': 'bananas_received',
     'wins': 'wins',
-    
-    # 'particle_trail': 'particle_trail',
+}
+
+DUELS = {
+    '_data': '_data',
+    'coins': 'coins',
+    'kills': 'kills',
+    'deaths': 'deaths',
+    'wins': 'wins',
+    'losses': 'losses',
+    'melee_hits': 'melee_hits',
+    'melee_swings': 'melee_swings',
+    'bow_hits': 'arrows_hit',
+    'bow_shots': 'arrows_shot',
+}
+
+BLITZ_DUELS = {
+    'blitz_duel_kills': 'kills',
+    'blitz_duel_deaths': 'deaths',
+    'blitz_duel_wins': 'wins',
+    'blitz_duel_losses': 'losses',
+    'blitz_duel_melee_hits': 'melee_hits',
+    'blitz_duel_melee_swings': 'melee_swings',
+    'blitz_duel_bow_hits': 'arrows_hit',
+    'blitz_duel_bow_shots': 'arrows_shot',
+}
+
+BOW_DUELS = {
+    'bow_duel_kills': 'kills',
+    'bow_duel_deaths': 'deaths',
+    'bow_duel_wins': 'wins',
+    'bow_duel_losses': 'losses',
+    # why if these are uncommented does it give overall duels data?
+    # 'bow_duel_melee_hits': 'melee_hits',
+    # 'bow_duel_melee_swings': 'melee_swings',
+    'bow_duel_bow_hits': 'arrows_hit',
+    'bow_duel_bow_shots': 'arrows_shot',
+}
+
+BOXING_DUELS = {
+    'boxing_duel_kills': 'kills',
+    'boxing_duel_deaths': 'deaths',
+    'boxing_duel_wins': 'wins',
+    'boxing_duel_losses': 'losses',
+    'boxing_duel_melee_hits': 'melee_hits',
+    'boxing_duel_melee_swings': 'melee_swings',
+    # 'boxing_duel_bow_hits': 'arrows_hit',
+    # 'boxing_duel_bow_shots': 'arrows_shot',
+}
+
+BRIDGE_DUELS = {
+    'bridge_duel_kills': 'kills',
+    'bridge_duel_deaths': 'deaths',
+    'bridge_duel_wins': 'wins',
+    'bridge_duel_losses': 'losses',
+    'bridge_duel_melee_hits': 'melee_hits',
+    'bridge_duel_melee_swings': 'melee_swings',
+    'bridge_duel_bow_hits': 'arrows_hit',
+    'bridge_duel_bow_shots': 'arrows_shot',
+}
+
+# TODO: other bridge modes
+
+CLASSIC_DUELS = {
+    'classic_duel_kills': 'kills',
+    'classic_duel_deaths': 'deaths',
+    'classic_duel_wins': 'wins',
+    'classic_duel_losses': 'losses',
+    'classic_duel_melee_hits': 'melee_hits',
+    'classic_duel_melee_swings': 'melee_swings',
+    'classic_duel_bow_hits': 'arrows_hit',
+    'classic_duel_bow_shots': 'arrows_shot',
+}
+
+COMBO_DUELS = {
+    'combo_duel_kills': 'kills',
+    'combo_duel_deaths': 'deaths',
+    'combo_duel_wins': 'wins',
+    'combo_duel_losses': 'losses',
+    'combo_duel_melee_hits': 'melee_hits',
+    'combo_duel_melee_swings': 'melee_swings',
+    # 'combo_duel_bow_hits': 'arrows_hit',
+    # 'combo_duel_bow_shots': 'arrows_shot',
+}
+
+MEGA_WALLS_DUELS = {
+    'mega_walls_duel_kills': 'kills',
+    'mega_walls_duel_deaths': 'deaths',
+    'mega_walls_duel_wins': 'wins',
+    'mega_walls_duel_losses': 'losses',
+    'mega_walls_duel_melee_hits': 'melee_hits',
+    'mega_walls_duel_melee_swings': 'melee_swings',
+    'mega_walls_duel_bow_hits': 'arrows_hit',
+    'mega_walls_duel_bow_shots': 'arrows_shot',
+}
+
+NO_DEBUFF_DUELS = {
+    'no_debuff_duel_kills': 'kills',
+    'no_debuff_duel_deaths': 'deaths',
+    'no_debuff_duel_wins': 'wins',
+    'no_debuff_duel_losses': 'losses',
+    'no_debuff_duel_melee_hits': 'melee_hits',
+    'no_debuff_duel_melee_swings': 'melee_swings',
+    # 'no_debuff_duel_bow_hits': 'arrows_hit',
+    # 'no_debuff_duel_bow_shots': 'arrows_shot',
+}
+
+OP_DUELS = {
+    'op_duel_kills': 'kills',
+    'op_duel_deaths': 'deaths',
+    'op_duel_wins': 'wins',
+    'op_duel_losses': 'losses',
+    'op_duel_melee_hits': 'melee_hits',
+    'op_duel_melee_swings': 'melee_swings',
+    'op_duel_bow_hits': 'arrows_hit',
+    'op_duel_bow_shots': 'arrows_shot',
+}
+
+PARKOUR_DUELS = {
+    'parkour_duel_kills': 'kills',
+    'parkour_duel_deaths': 'deaths',
+    'parkour_duel_wins': 'wins',
+    'parkour_duel_losses': 'losses',
+    # 'parkour_duel_melee_hits': 'melee_hits',
+    # 'parkour_duel_melee_swings': 'melee_swings',
+    # 'parkour_duel_bow_hits': 'arrows_hit',
+    # 'parkour_duel_bow_shots': 'arrows_shot',
+}
+
+SKYWARS_DUELS = {
+    'skywars_duel_kills': 'kills',
+    'skywars_duel_deaths': 'deaths',
+    'skywars_duel_wins': 'wins',
+    'skywars_duel_losses': 'losses',
+    'skywars_duel_melee_hits': 'melee_hits',
+    'skywars_duel_melee_swings': 'melee_swings',
+    'skywars_duel_bow_hits': 'arrows_hit',
+    'skywars_duel_bow_shots': 'arrows_shot',
+}
+
+# TODO: add other skywars gamemodes
+
+SUMO_DUELS = {
+    'sumo_duel_kills': 'kills',
+    'sumo_duel_deaths': 'deaths',
+    'sumo_duel_wins': 'wins',
+    'sumo_duel_losses': 'losses',
+    'sumo_duel_melee_hits': 'melee_hits',
+    'sumo_duel_melee_swings': 'melee_swings',
+    # 'sumo_duel_bow_hits': 'arrows_hit',
+    # 'sumo_duel_bow_shots': 'arrows_shot',
+}
+
+TNT_GAMES_DUELS = {
+    'tnt_games_duel_kills': 'kills',
+    'tnt_games_duel_deaths': 'deaths',
+    'tnt_games_duel_wins': 'wins',
+    'tnt_games_duel_losses': 'losses',
+    # 'tnt_games_duel_melee_hits': 'melee_hits',
+    # 'tnt_games_duel_melee_swings': 'melee_swings',
+    # 'tnt_games_duel_bow_hits': 'arrows_hit',
+    'tnt_games_duel_bow_shots': 'arrows_shot',
+}
+
+UHC_DUELS = {
+    'uhc_duel_kills': 'kills',
+    'uhc_duel_deaths': 'deaths',
+    'uhc_duel_wins': 'wins',
+    'uhc_duel_losses': 'losses',
+    'uhc_duel_melee_hits': 'melee_hits',
+    'uhc_duel_melee_swings': 'melee_swings',
+    'uhc_duel_bow_hits': 'arrows_hit',
+    'uhc_duel_bow_shots': 'arrows_shot',
 }
 
 SOCIALS = {
@@ -181,4 +381,12 @@ SOCIALS = {
     'TWITCH': 'twitch',
     'INSTAGRAM': 'instagram',
     'HYPIXEL': 'hypixel_forums',
+}
+
+BANS = {
+    'staff_rollingDaily': 'staff_day',
+    'staff_total': 'staff_total',
+    'watchdog_lastMinute': 'watchdog_recent',
+    'watchdog_rollingDaily': 'watchdog_day',
+    'watchdog_total': 'watchdog_total',
 }
