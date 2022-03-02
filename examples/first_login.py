b7ae3f2d9a65433b39b926c32242603a3851a21d
dubs <59372145+duhby@users.noreply.github.com>
2022-03-01 17:54:26 -0700

    Import errors

--- examples/first_login.py
+++ examples/first_login.py
@@ -1,6 +1,7 @@
 # This exmaple requies an api key
 
 import hypixel
+from hypixel.errors import HypixelException
 import asyncio
 
 description = """A simple example that shows how to get a player's first login."""
