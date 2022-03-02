acb470c2fb5e6bb30983c77d4e76b5b8912868ab
dubs <59372145+duhby@users.noreply.github.com>
2022-03-01 17:54:50 -0700

    Import errors

--- examples/key_queries.py
+++ examples/key_queries.py
@@ -1,6 +1,7 @@
 # This example requires an api key (obviously)
 
 import hypixel
+from hypixel.errors import HypixelException
 import asyncio
 
 description = """A simple example that shows how to get a key's queries."""
