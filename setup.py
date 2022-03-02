b697d6470c1888dc4e6a9be67347cb7e26d39a25
dubs <59372145+duhby@users.noreply.github.com>
2022-03-01 22:12:40 -0700

    Add [speed] extras_require

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 import re
 
-# regex filter courtesy of discord.py
 with open('hypixel/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
@@ -10,6 +9,19 @@ with open('README.md') as f:
 
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
+
+extras_require = {
+    "speed": [
+        "aiodns>=1.1",
+        "Brotlipy",
+        "cchardet",
+    ]
+}
+
+packages = [
+    'hypixel',
+    'hypixel.models',
+]
 
 classifiers = [
     #   3 - Alpha
@@ -21,11 +33,6 @@ classifiers = [
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3.7',
     'License :: OSI Approved :: MIT License',
-]
-
-packages = [
-    'hypixel',
-    'hypixel.models',
 ]
 
 setup(
