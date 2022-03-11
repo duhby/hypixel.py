1c89eb1845df048600466f6a99e32d817440c7a0
dubs <59372145+duhby@users.noreply.github.com>
2022-03-10 19:47:11 -0700

    Create ban_info.py

---
+++ examples/ban_info.py
@@ -0,0 +1,18 @@
+# This example requires an api key
+
+import hypixel
+from hypixel import HypixelException
+import asyncio
+
+description = """A simple example that shows how to get watchdog and staff ban info."""
+
+async def main():
+    client = hypixel.Client('api-key')
+    async with client:
+        try:
+            print(await client.bans())
+        except HypixelException as error:
+            print(error)
+
+if __name__ == '__main__':
+    asyncio.run(main())
