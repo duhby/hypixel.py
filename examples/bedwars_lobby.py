47c43d745b3b2200592be1746e3b7f823fcce7a4
dubs <59372145+duhby@users.noreply.github.com>
2022-03-10 21:00:39 -0700

    Add __doc__ to examples

--- examples/bedwars_lobby.py
+++ examples/bedwars_lobby.py
@@ -4,13 +4,13 @@ import hypixel
 from hypixel import HypixelException
 import asyncio
 
-description = """A simple example showing how to display a full bedwars lobby username."""
+__doc__ = """A simple example showing how to display a full bedwars lobby username."""
 
 async def main():
     client = hypixel.Client('api-key')
     async with client:
         try:
-            player = await client.player('duhby')
+            player = await client.player('gamerboy80')
             # will display [None] as the rank if the player has no rank
             # as it's meant to be a simple example, you can add your own logic
             print(f'[{player.bedwars.level}✫] [{player.rank}] {player.name}')
