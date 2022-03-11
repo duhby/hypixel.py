656121e18e74cfc2ba04dcde20c0f2261e36b6af
dubs <59372145+duhby@users.noreply.github.com>
2022-03-10 21:02:30 -0700

    Capitalize comments

--- examples/bedwars_lobby.py
+++ examples/bedwars_lobby.py
@@ -11,8 +11,8 @@ async def main():
     async with client:
         try:
             player = await client.player('gamerboy80')
-            # will display [None] as the rank if the player has no rank
-            # as it's meant to be a simple example, you can add your own logic
+            # Will display [None] as the rank if the player has no rank
+            # You can add your own logic
             print(f'[{player.bedwars.level}✫] [{player.rank}] {player.name}')
         except HypixelException as error:
             print(error)
