60a56d17c461ce75e224e607dd4a4b2fca4109dc
dubs <59372145+duhby@users.noreply.github.com>
2022-03-10 18:38:15 -0700

    Create converting_time.py

---
+++ examples/converting_time.py
@@ -0,0 +1,30 @@
+import hypixel
+from hypixel import HypixelException
+import asyncio
+
+try:
+    # Python 3.9+
+    from zoneinfo import ZoneInfo
+except ImportError:
+    # Python 3.7-8 (pip install backports.zoneinfo[tzdata])
+    # This module also works for python 3.6, however hypixel.py does not
+    from backports.zoneinfo import ZoneInfo
+
+description = """A simple example that shows how to convert the default time to other timezones."""
+
+async def main():
+    client = hypixel.Client('api-key')
+    async with client:
+        try:
+            player = await client.player('duhby')
+            # Can be None if in game privacy settings are changed
+            if player.last_login:
+                # List possible timezones with zoneinfo.available_timezones()
+                new_york = ZoneInfo('America/New_York')
+                # Convert UTC time to America/New_York time (accounts for daylight savings)
+                print(player.last_login.astimezone(new_york))
+        except HypixelException as error:
+            print(error)
+
+if __name__ == '__main__':
+    asyncio.run(main())
